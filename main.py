bd49edf0e68053477b3836c128f7c88fa5ed711c
winterdrive <90021888+winterdrive@users.noreply.github.com>
2024-07-26 15:38:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,52 +10,7 @@ from starlette.responses import StreamingResponse
 app = FastAPI()
 
 
-def fetch(url: str) -> str:
-    cloudscraper_mobile = cloudscraper.create_scraper(
-        delay=10,
-        browser={
-            "browser": "chrome",
-            "mobile": False,
-            "platform": "ios",
-            "custom": UserAgent().random,
-        },
-    )
-    response = cloudscraper_mobile.get(url, timeout=15)
-    response.raise_for_status()  # This will raise an exception for HTTP errors
-    return response.text
-
-
-def fetch_content(url: str) -> str:
-    cloudscraper_mobile = cloudscraper.create_scraper(
-        delay=10,
-        browser={
-            "browser": "chrome",
-            "mobile": False,
-            "platform": "ios",
-            "custom": UserAgent().random,
-        },
-    )
-    response = cloudscraper_mobile.get(url, timeout=15)
-    response.raise_for_status()  # This will raise an exception for HTTP errors
-    return response.content
-
-
-def fetch_content_utf8(url: str) -> str:
-    cloudscraper_mobile = cloudscraper.create_scraper(
-        delay=10,
-        browser={
-            "browser": "chrome",
-            "mobile": False,
-            "platform": "ios",
-            "custom": UserAgent().random,
-        },
-    )
-    response = cloudscraper_mobile.get(url, timeout=15)
-    response.raise_for_status()  # This will raise an exception for HTTP errors
-    return response.content.decode('utf-8')
-
-
-def fetch_content_binary(url: str) -> bytes:
+def fetch(url: str) -> bytes:
     cloudscraper_mobile = cloudscraper.create_scraper(
         delay=10,
         browser={
@@ -102,38 +57,11 @@ class URLItem(BaseModel):
     url: str
 
 
-@app.post("/fetch-url-content-binary")
-async def fetch_url_content_binary(item: URLItem):
-    try:
-        content = fetch_content_binary(item.url)
-        return StreamingResponse(io.BytesIO(content), media_type="text/html")
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
-
-
 @app.post("/fetch-url")
 async def fetch_url(item: URLItem):
     try:
         content = fetch(item.url)
-        return content
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
-
-
-@app.post("/fetch-url-content-utf8")
-async def fetch_url_content_utf8(item: URLItem):
-    try:
-        content = fetch_content_utf8(item.url)
-        return content
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
-
-
-@app.post("/fetch-url-content")
-async def fetch_url_content(item: URLItem):
-    try:
-        content = fetch_content(item.url)
-        return content
+        return StreamingResponse(io.BytesIO(content), media_type="text/html")
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
@@ -142,7 +70,7 @@ async def fetch_url_content(item: URLItem):
 def fetch_url_playwright(item: URLItem):
     try:
         content = fetch_content_by_playwright(item.url)
-        return content
+        return StreamingResponse(io.BytesIO(content), media_type="text/html")
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
